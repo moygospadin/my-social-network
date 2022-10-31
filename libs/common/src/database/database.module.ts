21013493c099e0549ddc4545afb57e2405374785
Aliaksei Bialenka <aliaksei.bialenka@innowise-group.com>
2022-11-01 00:31:42 +0100

    connect mongodb

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -5,9 +5,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-      }),
+      useFactory: (configService: ConfigService) => {
+        console.log('mongodb uri,', configService.get<string>('MONGODB_URI'));
+        return {
+          uri: configService.get<string>('MONGODB_URI'),
+        };
+      },
       inject: [ConfigService],
     }),
   ],
